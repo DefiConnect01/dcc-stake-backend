57df2388b558465c175a7f06c811fc4920eb165e
MAC <babatundeyusuffolorunsho@gmail.com>
2025-05-20 10:03:20 +0100

    adding frontend url

--- dist/config/allowedOrigins.js
+++ dist/config/allowedOrigins.js
@@ -4,4 +4,5 @@ exports.allowedOrigins = void 0;
 exports.allowedOrigins = [
     "http://localhost:8081",
     "http://localhost:5173",
+    "https://abundance-staking.vercel.app",
 ];
--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -1,4 +1,5 @@
 export const allowedOrigins = [
     "http://localhost:8081",
     "http://localhost:5173",
+    "https://abundance-staking.vercel.app",
   ];
